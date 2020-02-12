e687e0e29b7e92a075900c109863748f76fd1309
David Esteban Vargas Quevedo <david.vargas@decameron.com>
2020-02-12 07:30:36 -0500

    Se hace el llamado a la API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from "./Components/Header";
 import Formulario from "./Components/Formulario";
 
 function App() {
+
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect( () => {
+    const consultarAPI = async () => {
+      const key = 'bd7a30c95307495a974532b044a2a458';
+      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=${key}`;
+
+      const resultado = await fetch(url);
+      const noticias = await resultado.json();
+
+      guardarNoticias(noticias.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
+
   return (
     <Fragment>
       <Header
         titulo="Buscador de Noticias"
       />
       <div className="container white">
-        <Formulario/>
+        <Formulario
+          guardarCategoria={guardarCategoria}
+        />
       </div>
     </Fragment>
   );
