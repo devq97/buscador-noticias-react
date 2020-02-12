8f55f4bf12a07161a2431ba6660f789a6e690907
David Esteban Vargas Quevedo <david.vargas@decameron.com>
2020-02-12 07:30:56 -0500

    Se crean las opciones Se arma el select en el custom hook

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,12 +1,35 @@
 import React from "react";
 import styles from './Formulario.module.css';
+import useSelect from "../Hooks/useSelect";
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
+
+  const OPCIONES = [
+    {value: 'general', label: 'General'},
+    {value: 'business', label: 'Negocios'},
+    {value: 'entertaiment', label: 'Entretenimiento'},
+    {value: 'health', label: 'Salud'},
+    {value: 'science', label: 'Ciencia'},
+    {value: 'sports', label: 'Deportes'},
+    {value: 'technology', label: 'Tecnología'},
+  ];
+
+  const [categoria, SelecNoticias] = useSelect('general', OPCIONES);
+
+  const buscarNoticias = e => {
+    e.preventDefault();
+
+    guardarCategoria(categoria);
+  };
+
   return (
     <div className={`${styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
-        <form>
+        <form
+          onSubmit={buscarNoticias}
+        >
           <h2 className={styles.heading}>Encuentra noticias por categoría</h2>
+          <SelecNoticias/>
           <div className="input-field col s12">
             <input
               type="submit"
